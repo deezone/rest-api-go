aa3438f67e2566e80630f3cb7ba05553884d098e
Darren Douglas Lee <darren.lee@soul-cycle.com>
2017-12-24 16:12:59 -0500

    Stubs the GET /quotes and GET/POST/DELETE /quote/{id} endpoints

--- README.md
+++ README.md
@@ -7,12 +7,17 @@ $ go run main.go
 ```
 
 #### Endpoints
-- GET `/quote (GET)` -> All quotes in the quotes document (database)
+- GET `/quotes` -> All quotes in the quotes document (database)
 - GET `/quote/{id}` -> Get a single quote
 - POST `/quote/{id}` -> Create a new quote
 - DELETE `/quote/{id}` -> Delete a quote
 
 #### References
+##### API Application in Go
 - [Building a RESTful API with Golang](https://www.codementor.io/codehakase/building-a-restful-api-with-golang-a6yivzqdo)
+- [API foundations in Go](https://leanpub.com/api-foundations)
+
+##### Go Development
 - [Your First Program](https://www.golang-book.com/books/intro/2)
 - [Dep](https://github.com/golang/dep): dependency management tool for Go
+- [Godoc: documenting Go code](https://blog.golang.org/godoc-documenting-go-code)
--- main.go
+++ main.go
@@ -6,8 +6,26 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// our main function
+// main function
 func main() {
 	router := mux.NewRouter()
-	log.Fatal(http.ListenAndServe(":8000", router))
+
+	router.HandleFunc("/quote", GetQuotes).Methods("GET")
+	router.HandleFunc("/quote/{id}", GetQuote).Methods("GET")
+	router.HandleFunc("/quote/{id}", CreateQuote).Methods("POST")
+	router.HandleFunc("/quote/{id}", DeleteQuote).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8001", router))
 }
+
+// GetQuotes looks up all of the quotes.
+func GetQuotes(w http.ResponseWriter, r *http.Request) {}
+
+// GetQuote looks up a specific quote by ID.
+func GetQuote(w http.ResponseWriter, r *http.Request) {}
+
+// CreateQuote creates a new quote.
+func CreateQuote(w http.ResponseWriter, r *http.Request) {}
+
+// DeleteQuote deletes a quote by quote ID.
+func DeleteQuote(w http.ResponseWriter, r *http.Request) {}
