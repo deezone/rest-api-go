41adecb63757586fd0a3ec94c1bbdaac2f70c6ec
Darren Douglas Lee <darren.lee@soul-cycle.com>
2017-12-27 12:51:34 -0500

    Adds GoDoc details by using correct formatting and provide references in README. Adds DeleteQuote functionality

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
+[![GoDoc](https://godoc.org/github.com/DeeZone/rest-api-go?status.svg)](https://godoc.org/github.com/DeeZone/rest-api-go)
+
 # rest-api-go
-REST API using the Go programming language.
+REST API using the Go programming language. A learning excercise to understand the use of the Go programming language 
+when building an API.
 
 #### To Start
 ```
@@ -12,42 +15,8 @@ $ go run main.go
 - POST `/quote/{id}` -> Create a new quote
 - DELETE `/quote/{id}` -> Delete a quote
 
-**POST Example**
-- raw `JSON(application/json)`
-```
-{
-    "id": 6,
-    "quote": "Test quote.",
-    "authour": {
-        "id": 6,
-        "first": "First",
-        "last": "Last",
-        "born": "2000-01-02T00:00:00Z",
-        "died": "2010-01-03T00:00:00Z",
-        "description": "Test description.",
-        "biolink": "http://somesite.com"
-    }
-}
-```
-
-#### References
-##### API Application in Go
-- [Building a RESTful API with Golang](https://www.codementor.io/codehakase/building-a-restful-api-with-golang-a6yivzqdo)
-- [API foundations in Go](https://leanpub.com/api-foundations)
-
-##### Go Development
-- [Your First Program](https://www.golang-book.com/books/intro/2)
-- [Dep](https://github.com/golang/dep): dependency management tool for Go
-- [Godoc: documenting Go code](https://blog.golang.org/godoc-documenting-go-code)
-- [Manage config in Golang to get variables from file and env variables](https://medium.com/@felipedutratine/manage-config-in-golang-to-get-variables-from-file-and-env-variables-33d876887152)
-- [Efficient String Concatenation in Go](http://herman.asia/efficient-string-concatenation-in-go)
-
-##### Packages
-- [time](https://golang.org/pkg/time/)
-- [encoding/json](https://golang.org/pkg/encoding/json/)
-- [strconv](https://golang.org/pkg/strconv/)
-- [strings](https://golang.org/pkg/strings/)
-- [os](https://golang.org/pkg/os/)
+#### Documentation
+- [GoDoc Documentation](https://godoc.org/github.com/DeeZone/rest-api-go)
 
 #### Configuration
 
@@ -71,3 +40,46 @@ Add configuration files to:
 
 A combination of the environment variable and configuration file settings will be used by the application to determine
 run time settings.
+
+#### Development Examples
+
+- POST `/quote/{id}` -> Create a new quote
+
+**_Example_**
+
+`POST /quote/6`
+- raw `JSON(application/json)`
+```
+{
+    "quote": "Test quote.",
+    "authour": {
+        "id": 6,
+        "first": "First",
+        "last": "Last",
+        "born": "2000-01-02T00:00:00Z",
+        "died": "2010-01-03T00:00:00Z",
+        "description": "Test description.",
+        "biolink": "http://somesite.com"
+    }
+}
+```
+
+#### References
+##### API Application in Go
+- [Building a RESTful API with Golang](https://www.codementor.io/codehakase/building-a-restful-api-with-golang-a6yivzqdo)
+- [API foundations in Go](https://leanpub.com/api-foundations)
+
+##### Go Development
+- [Your First Program](https://www.golang-book.com/books/intro/2)
+- [Dep](https://github.com/golang/dep): dependency management tool for Go
+- [Manage config in Golang to get variables from file and env variables](https://medium.com/@felipedutratine/manage-config-in-golang-to-get-variables-from-file-and-env-variables-33d876887152)
+- [Efficient String Concatenation in Go](http://herman.asia/efficient-string-concatenation-in-go)
+- [Documenting Go Code With Godoc](https://www.goinggo.net/2013/06/documenting-go-code-with-godoc.html)
+- [Godoc: documenting Go code](https://blog.golang.org/godoc-documenting-go-code)
+
+##### Packages
+- [time](https://golang.org/pkg/time/)
+- [encoding/json](https://golang.org/pkg/encoding/json/)
+- [strconv](https://golang.org/pkg/strconv/)
+- [strings](https://golang.org/pkg/strings/)
+- [os](https://golang.org/pkg/os/)
--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// A learning excercise to understand the use of the Go programming language when building an API
+// Started Christmas 2017
+// Governed by the license that can be found in the LICENSE file
 package main
 
 import (
@@ -13,20 +16,20 @@ import (
 	"os"
 )
 
-// Configuration settings common to application
+// Configuration type settings common to application
 type Configuration struct {
 	Port        int
 	Environment string
 }
 
-// The quote Type (more like an object)
+// The Quote type (more like an object) that manages the details of a quote.
 type Quote struct {
 	ID      int      `json:"id"`
 	Quote   string   `json:"quote"`
 	Authour *Authour `json:"authour,omitempty"`
 }
 
-// Authour type, referenced by core items: quotes, publications, etc
+// Authour type, referenced by core items: quotes, publications, etc.
 type Authour struct {
 	ID          int       `json:"id"`
 	First       string    `json:"first,omitempty"`
@@ -40,23 +43,27 @@ type Authour struct {
 var quotes []Quote
 
 // GetQuotes looks up all of the quotes.
+// GET /quotes
+// Returns all of the quotes in the JSON format.
 func GetQuotes(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(quotes)
 }
 
 // GetQuote looks up a specific quote by ID.
+// GET /quote/{id}
+// Returns a quote in the JSON format provided the target ID is valid.
 func GetQuote(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	params := mux.Vars(r)
-	targetId, err := strconv.Atoi(params["id"])
+	targetID, err := strconv.Atoi(params["id"])
 	if (err != nil) {
 		io.WriteString(w, `{"status": "error, invalid ID"}`)
 		return
 	}
 
 	for _, item := range quotes {
-		if item.ID == targetId {
+		if item.ID == targetID {
 			json.NewEncoder(w).Encode(item)
 			return
 		}
@@ -65,11 +72,13 @@ func GetQuote(w http.ResponseWriter, r *http.Request) {
 }
 
 // CreateQuote creates a new quote.
+// POST /quote/{id}
+// Returns all quotes including the newly added quote made by the POST request.
 func CreateQuote(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	params := mux.Vars(r)
-	targetId, err := strconv.Atoi(params["id"])
+	targetID, err := strconv.Atoi(params["id"])
 	if (err != nil) {
 		io.WriteString(w, `{"status": "error, invalid ID"}`)
 		return
@@ -77,15 +86,31 @@ func CreateQuote(w http.ResponseWriter, r *http.Request) {
 
 	var quote Quote
 	_ = json.NewDecoder(r.Body).Decode(&quote)
-	quote.ID = targetId
+	quote.ID = targetID
 	quotes = append(quotes, quote)
 	json.NewEncoder(w).Encode(quotes)
 }
 
 // DeleteQuote deletes a quote by quote ID.
+// DELETE /quote/{id}
+// Returns all quotes which will exclude the deleted quote made by the DELETE request.
 func DeleteQuote(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	io.WriteString(w, `{"status":"ok"}`)
+
+	params := mux.Vars(r)
+	targetID, err := strconv.Atoi(params["id"])
+	if (err != nil) {
+		io.WriteString(w, `{"status": "error, invalid ID"}`)
+		return
+	}
+
+	for index, item := range quotes {
+		if item.ID == targetID {
+			quotes = append(quotes[:index], quotes[:index+1]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(quotes)
 }
 
 // main function
@@ -97,7 +122,7 @@ func main() {
 	err := gonfig.GetConf(strings.Join(env, ""), &configuration)
 	if (err != nil) { return }
 
-	LoadData()
+	quotes = LoadData()
 
 	router := mux.NewRouter()
 
@@ -111,9 +136,13 @@ func main() {
 	log.Fatal(http.ListenAndServe(strings.Join(port, ""), router))
 }
 
-// LoadData populates the quotes array with initial data store values to mimic a database
-func LoadData() {
-	quotes = append(quotes, Quote{
+// LoadData manages the gathering of quote data.
+// Used as data store to mimic a database
+// Returns an array of quotes
+func LoadData() []Quote {
+	var data []Quote
+
+	data = append(data, Quote{
 		ID: 1,
 		Quote: "When it's pouring rain and you're bowling along through the wet, there's satisfaction in knowing you're out there and the others aren't.",
 		Authour: &Authour{
@@ -123,7 +152,7 @@ func LoadData() {
 			Born: time.Date(1938, time.December, 17, 0, 0, 0, 0, time.UTC),
 			Description: "A New Zealand former middle-distance runner. He won three Olympic gold medals, and is the only male since 1920 to win the 800 and 1500 metres at the same Olympics, in 1964.",
 			BioLink:"https://en.wikipedia.org/wiki/Peter_Snell"}})
-	quotes = append(quotes, Quote{
+	data = append(data, Quote{
 		ID: 2,
 		Quote: "I run because it's my passion, and not just a sport. Every time I walk out the door, I know why I'm going where I'm going and I'm already focused on that special place where I find my peace and solitude. Running, to me, is more than just a physical exercise... it's a consistent reward for victory!",
 		Authour: &Authour{
@@ -133,7 +162,7 @@ func LoadData() {
 			Born: time.Date(1978, time.May, 20, 0, 0, 0, 0, time.UTC),
 			Description: "Modeling, acting and photography",
 			BioLink:"http://www.imdb.com/name/nm1659315/bio"}})
-	quotes = append(quotes, Quote{
+	data = append(data, Quote{
 		ID: 3,
 		Quote: "If you always put limits on what you can do, physical or anything else, it'll spread over into the rest of your life. It'll spread into your work, into your morality, into your entire being. There are no limits. There are plateaus, but you must not stay there, you must go beyond them.",
 		Authour: &Authour{
@@ -144,7 +173,7 @@ func LoadData() {
 			Died: time.Date(1973, time.July, 20, 0, 0, 0, 0, time.UTC),
 			Description: "A Hong Kong and American actor, film director, martial artist, martial arts instructor, philosopher[5] and founder of the martial art Jeet Kune Do.",
 			BioLink:"https://en.wikipedia.org/wiki/Bruce_Lee"}})
-	quotes = append(quotes, Quote{
+	data = append(data, Quote{
 		ID: 4,
 		Quote: "Motivation is what gets you started. Habit is what keeps you going.",
 		Authour: &Authour{
@@ -154,7 +183,7 @@ func LoadData() {
 			Born: time.Date(1947, time.April, 29, 0, 0, 0, 0, time.UTC),
 			Description: "A former American politician and track and field athlete. He won a silver medal in the men's 1500 metres at the 1968 Summer Olympics, and was the first high school athlete to run a mile in under four minutes.",
 			BioLink:"https://en.wikipedia.org/wiki/Jim_Ryun"}})
-	quotes = append(quotes, Quote{
+	data = append(data, Quote{
 		ID: 5,
 		Quote: "I don't think about the miles that are coming down the road, I don't think about the mile I'm on right now, I don't think about the miles I've already covered. I think about what I'm doing right now, just being lost in the moment.",
 		Authour: &Authour{
@@ -164,4 +193,6 @@ func LoadData() {
 			Born: time.Date(1982, time.October, 14, 0, 0, 0, 0, time.UTC),
 			Description: "U.S. Olympic marathoner",
 			BioLink:"https://en.wikipedia.org/wiki/Ryan_Hall_(runner)"}})
+
+	return data
 }
